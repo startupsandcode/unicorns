ee4ef52b3ff72a3ee0ed9e8c4b168e8c9e4aa78e
jmann-ts <john@travelspike.com>
2016-06-02 09:21:17 -0400

    added unicorn color and pics and made a table to keep a stable layout

--- gulpfile.js
+++ gulpfile.js
@@ -12,35 +12,42 @@ var config = {
         html: './src/*.html',
         js: './src/scripts/*.js',
         less: './src/less/*.less',
+        images: './src/images/**/*',
         dist: './dist'
     }
 }
 
-gulp.task('html',function(){
+gulp.task('html', function () {
     gulp.src(config.paths.html)
         .pipe(gulp.dest(config.paths.dist))
         .pipe(connect.reload());
 })
 
-gulp.task('less',function(){
+gulp.task('less', function () {
     gulp.src(config.paths.less)
         .pipe(less())
         .pipe(gulp.dest(config.paths.dist + '/css'))
         .pipe(connect.reload());
 })
 
-gulp.task('js', function(){
+gulp.task('js', function () {
     gulp.src(config.paths.js)
         .pipe(gulp.dest(config.paths.dist + '/scripts'))
         .pipe(connect.reload());
 })
 
-gulp.task('open', ['connect'], function () {
-	gulp.src('dist/index.html')
-		.pipe(open({ uri: config.baseUrl + ':' + config.port + '/' }));
+gulp.task('images', function () {
+    gulp.src(config.paths.images)
+        .pipe(gulp.dest(config.paths.dist + '/images'))
+        .pipe(connect.reload());
 })
 
-gulp.task('connect', function(){
+gulp.task('open', ['connect'], function () {
+    gulp.src('dist/index.html')
+        .pipe(open({ uri: config.baseUrl + ':' + config.port + '/' }));
+})
+
+gulp.task('connect', function () {
     connect.server({
         root: ['dist'],
         port: config.port,
@@ -50,9 +57,9 @@ gulp.task('connect', function(){
 });
 
 gulp.task('watch', function () {
-	gulp.watch(config.paths.html, ['html']);
-	gulp.watch(config.paths.js, ['js']);
+    gulp.watch(config.paths.html, ['html']);
+    gulp.watch(config.paths.js, ['js']);
     gulp.watch(config.paths.less, ['less']);
 });
 
-gulp.task('default',['html','less','js', 'open', 'watch']);
+gulp.task('default', ['images', 'html', 'less', 'js', 'open', 'watch']);
--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,35 +1,40 @@
 
 //Globals
-var locations = ['corral','pasture','barn'];
+var locations = ['corral', 'pasture', 'barn'];
+var colors = ['red','yellow','green','blue','purple','white','grey'];
 var allUnicorns = [];
 
 //Unicorn Constructor
-function Unicorn(id, name,color,location){
-        this.id =  id
-        this.name = name
-        this.color = color
-        this.location = location;
+function Unicorn(id, name, color, location) {
+    this.id = id
+    this.name = name
+    this.color = color
+    this.location = location;
 }
 
 //Generate random location from array
-var currentLocation = function(){
+var currentLocation = function () {
     return Math.floor(Math.random() * locations.length);
 }
 
+var currentColor = function(){
+    return Math.floor(Math.random() * colors.length);
+}
+
 //Generate 13 new Unicorns
-for (var i = 0; i < 14; i++){
-    allUnicorns.push(new Unicorn(i, 'Tom - ' + i,'Color ' + i,locations[currentLocation()]));
+for (var i = 0; i < 14; i++) {
+    allUnicorns.push(new Unicorn(i, 'Tom - ' + i, colors[currentColor()], locations[currentLocation()]));
 }
 
 //Create dropdown of locations
-function showLocations(current){
+function showLocations(current) {
     var locationList = document.createElement('select');
-    locationList.addEventListener('change',updateLocation);
-    for(var i = 0; i < locations.length; i++){
+    locationList.addEventListener('change', updateLocation);
+    for (var i = 0; i < locations.length; i++) {
         var currentOption = document.createElement('option');
         currentOption.value = locations[i];
         currentOption.text = locations[i];
-        if (current === locations[i]){
+        if (current === locations[i]) {
             currentOption.selected = true;
         }
         locationList.appendChild(currentOption);
@@ -38,30 +43,33 @@ function showLocations(current){
 }
 
 //Update Location of unicorn
-function updateLocation(event){
-    var id = event.currentTarget.parentElement.id;
+function updateLocation(event) {
+    var id = event.currentTarget.parentElement.parentElement.id;
     allUnicorns[id].location = event.currentTarget.value;
     //Refreshes display on update
     displayUnicornInformation();
 }
 
 //Show information of unicorns
-function displayUnicornInformation(){
-    var list = document.getElementById('unicorns');
-    list.innerHTML = '';
-    allUnicorns.forEach(function(currentUnicorn){
-        var uni = document.createElement('li');
+function displayUnicornInformation() {
+    var unicornTable = document.getElementById('unicorns');
+    unicornTable.innerHTML = '';
+    allUnicorns.forEach(function (currentUnicorn) {
+        var uni = document.createElement('tr');
         var locationList = showLocations(currentUnicorn.location);
-        uni.innerHTML =  '<span class="unicornStatus">' + currentUnicorn.name + ' : ' + currentUnicorn.color + ' : ' + currentUnicorn.location + '</span>';
+        unicornData = '<td class="unicornData" style="background-color:' + currentUnicorn.color + '"></td><td class="unicornStatus">' + currentUnicorn.name + '</td><td>' + currentUnicorn.color + '</td><td>' + currentUnicorn.location + '</td>';
+        var locationTd = document.createElement('td');
+        locationTd.appendChild(locationList);
+        uni.innerHTML = unicornData;
         uni.id = currentUnicorn.id;
-        uni.appendChild(locationList);
-        list.appendChild(uni); 
+        uni.appendChild(locationTd);
+        unicornTable.appendChild(uni);
     })
 }
 
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
     //Initial display of unicorn information
-    displayUnicornInformation();    
+    displayUnicornInformation();
 })
 
 
