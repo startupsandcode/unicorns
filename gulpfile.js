81c88297d21723f26aba825c5b12de0abcc2773d
jmmann <jmmann@hugeinc.com>
2018-06-13 15:08:05 -0400

    updated for gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,3 @@
-"use strict";
-
 var gulp = require('gulp');
 var less = require('gulp-less');
 var connect = require('gulp-connect');
@@ -18,48 +16,43 @@ var config = {
 }
 
 gulp.task('html', function () {
-    gulp.src(config.paths.html)
+    return gulp.src(config.paths.html)
         .pipe(gulp.dest(config.paths.dist))
         .pipe(connect.reload());
 })
 
 gulp.task('less', function () {
-    gulp.src(config.paths.less)
+    return gulp.src(config.paths.less)
         .pipe(less())
         .pipe(gulp.dest(config.paths.dist + '/css'))
         .pipe(connect.reload());
 })
 
 gulp.task('js', function () {
-    gulp.src(config.paths.js)
+    return gulp.src(config.paths.js)
         .pipe(gulp.dest(config.paths.dist + '/scripts'))
         .pipe(connect.reload());
 })
 
 gulp.task('images', function () {
-    gulp.src(config.paths.images)
+    return gulp.src(config.paths.images)
         .pipe(gulp.dest(config.paths.dist + '/images'))
         .pipe(connect.reload());
 })
 
-gulp.task('open', ['connect'], function () {
-    gulp.src('dist/index.html')
-        .pipe(open({ uri: config.baseUrl + ':' + config.port + '/' }));
-})
-
 gulp.task('connect', function () {
-    connect.server({
+    return connect.server({
         root: ['dist'],
         port: config.port,
         base: config.baseUrl,
         livereload: true
     })
-});
+})
 
-gulp.task('watch', function () {
-    gulp.watch(config.paths.html, ['html']);
-    gulp.watch(config.paths.js, ['js']);
-    gulp.watch(config.paths.less, ['less']);
-});
+gulp.task('open', gulp.series('connect', function () {
+    return gulp.src('dist/index.html')
+        .pipe(open({ uri: config.baseUrl + ':' + config.port + '/' }));
+}))
 
-gulp.task('default', ['images', 'html', 'less', 'js', 'open', 'watch']);
+
+gulp.task('default', gulp.series('images', 'html', 'less', 'js', 'open'));
